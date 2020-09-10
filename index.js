adbaa2b69e17bf64bfa02a0f56f9fd561d6bdc6e
LouisLec <louis.l@obole-digitale.fr>
2020-09-10 12:40:15 +0200

    fix: forgot to remove plugin + prettier

--- index.js
+++ index.js
@@ -1,40 +1,39 @@
-require('dotenv').config()
-const express = require('express')
-const { postgraphile } = require('postgraphile')
+require("dotenv").config();
+const express = require("express");
+const { postgraphile } = require("postgraphile");
 const ConnectionFilterPlugin = require("postgraphile-plugin-connection-filter");
 
-const app = express()
+const app = express();
 
 app.use(
   postgraphile(process.env.DATABASE_URL, process.env.SCHEMA, {
     subscriptions: true,
-    skipPlugins:[
-      require('graphile-build').NodePlugin
-    ],
+    skipPlugins: [require("graphile-build").NodePlugin],
     appendPlugins: [
-      messageSubscriptionPlugin,
       require("postgraphile/plugins").TagsFilePlugin,
-      require('@graphile-contrib/pg-simplify-inflector'),
+      require("@graphile-contrib/pg-simplify-inflector"),
       ConnectionFilterPlugin,
     ],
     dynamicJson: true,
     enableCors: true,
     enableQueryBatching: true,
     enhanceGraphiql: true,
-    extendedErrors: ['hint', 'detail', 'errcode'],
+    extendedErrors: ["hint", "detail", "errcode"],
     graphiql: true,
     ignoreIndexes: false,
     ignoreRBAC: false,
     // jwtPgTypeIdentifier: 'app_public.jwt_token',
     // jwtSecret: 'SuperSecret!',
     // pgDefaultRole: 'capi_anon',
-    legacyRelations: 'omit',
+    legacyRelations: "omit",
     setofFunctionsContainNulls: false,
-    showErrorStack: 'json',
+    showErrorStack: "json",
     subscriptions: true,
     watchPg: process.env.NODE_ENV == "development",
-    allowExplain(req) { },
+    allowExplain(req) {},
   })
 );
 
-app.listen(process.env.PORT, () => console.log("Server listening on port " + process.env.PORT));
+app.listen(process.env.PORT, () =>
+  console.log("Server listening on port " + process.env.PORT)
+);
